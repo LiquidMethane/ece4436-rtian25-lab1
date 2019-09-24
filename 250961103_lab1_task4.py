512d21c3eb6c83d5b508064148dd28bbf236de6c
Ruiqi Tian <rtian25@uwo.ca>
2019-09-23 22:51:39 -0400

    establish connection with gmail server

--- 250961103_lab1_task4.py
+++ 250961103_lab1_task4.py
@@ -1,33 +1,38 @@
 from socket import *
 
-msg = "\r\n I love computer networks!" endmsg = "\r\n.\r\n"
+msg = "\r\n I love computer networks!" 
+endmsg = "\r\n.\r\n"
 
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver mailserver = #Fill in start #Fill in end
-
-
+mailserver = 'smtp.gmail.com'
+port = 587
 # Create socket called clientSocket and establish a TCP connection with mailserver
 
 #Fill in start
-
-
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver, port))
 
 
 #Fill in end
 
-recv = clientSocket.recv(1024).decode() print(recv)
+recv = clientSocket.recv(1024).decode() 
+print(recv)
 if recv[:3] != '220':
-
-print('220 reply not received from server.')
-
+    print('220 reply not received from server.')
 
 
 
-# Send HELO command and print server response. heloCommand = 'HELO Alice\r\n' clientSocket.send(heloCommand.encode())
-recv1 = clientSocket.recv(1024).decode() print(recv1)
+
+# Send HELO command and print server response. 
+
+heloCommand = 'HELO Alice\r\n' 
+clientSocket.send(heloCommand.encode())
+
+recv1 = clientSocket.recv(1024).decode() 
+print(recv1)
 if recv1[:3] != '250':
-
-print('250 reply not received from server.')
+    print('250 reply not received from server.')
 
 
 
